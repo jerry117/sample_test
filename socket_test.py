c187e8e9130b8d233b4daf967a797f2f9d26d740
jerry117 <614237720@qq.com>
2016-12-28 20:12:21 +0800

    change the comment syntax
    change the comment syntax

--- socket_test.py
+++ socket_test.py
@@ -2,7 +2,7 @@
 import socket
 HOST = 'localhost'
 PORT = 843
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #使用的通信方式
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #用什么方式通信，使用的套接字类型
 s.connect((HOST,PORT)) #连接host和post
 
 # 组装信息

4bcaad9fca8c78582b450c9ad70261f507b1eafc
jerry <614237720@qq.com>
2017-04-27 00:55:51 +0800

    pymysql_test
    connect mysql by pymysql

---
+++ test_pymysql.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+import pymysql.cursors
+
+# connect to the database
+connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='asdf', db='test', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+try:
+    with connection.cursor() as cursor:
+        sql = 'INSERT INTO sign_guest (realname, phone, email, sign, event_id, create_time) VALUES ("alen",18800110001,"alen@mail.com",0,1,NOW());'
+        cursor.execute(sql)
+    connection.commit()
+    with connection.cursor() as cursor:
+        sql = "SELECT realname,phone,email,sign FROM sign_guest WHERE phone=%s"
+        cursor.execute(sql, ('18800110001',))
+        result = cursor.fetchone()
+        print result
+    connection.commit()
+
+finally:
+    connection.close()

5940a990a5da199acdabf1c146c8981345721f25
lijialiang <614237720@qq.com>
2022-11-18 09:52:56 +0800

    feat: fix

--- python_abort.py
+++ python_abort.py
@@ -9,10 +9,13 @@ def demo():
     if not request.args.get('user'):
         abort(400)  # 400 bad request
     else:
-        res = Response('hello world')
+        res = Response(f"hello world!{request.args.get('user')}")
         return abort(res)
 
 
 if __name__ == '__main__':
     pprint.pprint(werkzeug.exceptions.default_exceptions) #异常列表
     app.run(debug=True)
+
+# 运行方法
+# gunicorn -w 2 -b 0.0.0.0:8001 python_abort:app
